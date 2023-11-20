67ceb8b1981b0daa41879f89d71acbad99aab883
ginoya <rishitginoya@gmail.com>
2023-11-20 16:28:53 +0530

    Initial commit

---
+++ ConsoleReader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    public delegate void OnWord(string word);
+
+    public delegate void OnNumber(int number);
+
+    public delegate void OnJunk(string junk);
+
+    public static class ConsoleReader
+    {
+        public static void readFromConsole(OnWord onWord, OnNumber onNumber, OnJunk onJunk)
+        {
+            Console.WriteLine("Please enter text : ");
+            string inputWord = Console.ReadLine();
+            if (!string.IsNullOrEmpty(inputWord))
+            {
+                if (inputWord.All(char.IsLetter))
+                {
+                    onWord(inputWord);
+                }
+                else if (inputWord.All(char.IsDigit))
+                {
+                    onNumber(int.Parse(inputWord));
+                }
+                else
+                {
+                    onJunk(inputWord);
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleReader.readFromConsole(OnWord, OnNumber, OnJunk);
+        }
+        public static void OnJunk(string junk)
+        {
+            Console.WriteLine("OnJunk called! as user input string is : {0}", junk);
+        }
+
+        public static void OnWord(string word)
+        {
+            Console.WriteLine("OnWord called! as user input string is : {0}", word);
+        }
+
+        public static void OnNumber(int number)
+        {
+            Console.WriteLine("OnNumber called! as user input string is : {0}", number);
+        }
+
+    }
+}
